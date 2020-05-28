654eac64a627d2a9e9af98ec97fc4f2e462a5efe
nikkiricks <nikki.ricks@gmail.com>
2020-05-28 21:00:52 +1000

    first commit

---
+++ lizzo.js
@@ -0,0 +1,119 @@
+
+// process.stdout.write(`I\n`)
+// process.stdout.write(`\n`)
+// process.stdout.write(`just\n`)
+// process.stdout.write(`took\n`)
+
+setTimeout(I, 500)
+
+function I() {
+  console.log(`I \n`)
+}
+
+setTimeout(just, 1000)
+
+function just() {
+  console.log(`just \n`)
+}
+
+setTimeout(took, 1500)
+
+function took() {
+  console.log(`took \n`)
+}
+
+setTimeout(a, 2000)
+
+function a() {
+  console.log(`a \n`)
+}
+
+setTimeout(d, 2500)
+
+function d() {
+  console.log(`d \n`)
+}
+
+setTimeout(n, 3000)
+
+function n() {
+  console.log(`n \n`)
+}
+
+setTimeout(dnaA, 3500)
+
+function dnaA() {
+  console.log(`a \n`)
+}
+
+setTimeout(test, 4500)
+
+function test() {
+  console.log(`test \n`)
+}
+
+setTimeout(turns, 5500)
+
+function turns() {
+  console.log(`turns \n`)
+}
+
+setTimeout(out, 6500)
+
+function out() {
+  console.log(`out \n`)
+}
+
+setTimeout(im, 7000)
+
+function im() {
+  console.log(`I'm \n`)
+}
+
+setTimeout(one, 7500)
+
+function one() {
+  console.log(`one \n`)
+}
+
+setTimeout(hun, 8000)
+
+function hun() {
+  console.log(`hun \n`)
+}
+
+setTimeout(dred, 8500)
+
+function dred() {
+  console.log(`dred \n`)
+}
+
+setTimeout(per, 9000)
+
+function per() {
+  console.log(`per \n`)
+}
+
+setTimeout(cent, 10000)
+
+function cent() {
+  console.log(`cent \n`)
+}
+
+setTimeout(that, 11000)
+
+function that() {
+  console.log(`that \n`)
+}
+
+setTimeout(bitch, 12000)
+
+function bitch() {
+  console.log(`bitch \n`)
+}
+
+setTimeout(pause, 20000)
+
+function pause() {
+  console.log(`✌️ \n`)
+}
